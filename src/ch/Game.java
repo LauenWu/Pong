967b33092e62aac2174385058f25f55e71463676
l_wue <lauen.wuergler@subsidia.ch>
2020-09-12 09:42:57 +0200

    init

--- src/ch/Game.java
+++ src/ch/Game.java
@@ -1,5 +1,98 @@
 package ch;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+import ch.backend.Handler;
+
+public class Game extends Canvas implements Runnable {
 	
+	public static final int WIDTH = 800, HEIGHT = WIDTH/12*9;
+    private Thread thread;
+    private boolean running = false;
+    private Handler handler;
+
+	public synchronized void start(){
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    public synchronized void stop(){
+        try {
+            thread.join();
+            running = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run(){
+        //game-loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while (running){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                delta--;
+            }
+            if(running){
+                render();
+            }
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick(){
+        handler.tick();
+
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.YELLOW);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        handler.render(g);
+
+        g.dispose();
+        bs.show();
+    }
+
+    public static int clamp(int var, int max, int min){
+        if(var <= min){
+            return min;
+        }else if(var >= max){
+            return max;
+        }else{
+            return var;
+        }
+    }
+
+    public static void main(String args[]){
+        new Game();
+    }
+
 }
